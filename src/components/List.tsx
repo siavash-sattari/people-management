7df4a5d48af85c2217fafc8701d23d0b19e1842f
siavash sattari <siavash.sattari.75@gmail.com>
2022-04-30 04:20:24 -0700

    List of people

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -13,7 +13,30 @@ interface IProps {
 }
 
 const List: FC<IProps> = ({ people }) => {
-  return <div>List</div>;
+  const renderList: JSX.Element[] = people.map(p => (
+    <div className='col-12 col-lg-6'>
+      <div className='card'>
+        <div className='card-body d-flex align-items-center'>
+          <img
+            className='img-fluid rounded img-thumbnail'
+            width={100}
+            height={100}
+            src={p.img_url}
+            alt={p.fullName}
+          />
+          <div className='mx-3'>
+            <p>
+              <span className='h2'>{p.fullName}</span>
+              <span className='badge bg-primary me-3'>سال {p.age}</span>
+            </p>
+            <p className='text-muted'>{p.bio}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  ));
+
+  return <div className='row'>{renderList}</div>;
 };
 
 export default List;
